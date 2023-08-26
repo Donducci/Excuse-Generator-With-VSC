27745eb405c9c8615e8a81f0ce805d912e93ea71
Donducci <121743831+Donducci@users.noreply.github.com>
2023-08-25 20:47:06 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+const button = document.querySelector("#btn");
+button.addEventListener("click", () => {
+  document.querySelector("#the-excuse");
+});
+window.onload = function() {
+  /* eslint-disable */
+  const paragraph = document.querySelector("#the-excuse");
+  let pronoun = ["A", "The"];
+  let subject = ["teacher", "goat", "cat", "driver", "mechanic"];
+  let action = ["took my", "eat my", "tore my", "drove my", "broke my"];
+  let object = ["homework", "banana", "clothe", "car", "ratchet"];
+  3;
+  let location = [
+    "in the classroom",
+    "in my farm",
+    "in my house",
+    "im my garage",
+    "in my workshop"
+  ];
+
+  let proindx = Math.floor(Math.random() * pronoun.length);
+  let subindx = Math.floor(Math.random() * subject.length);
+  let actindx = Math.floor(Math.random() * action.length);
+  let objindx = Math.floor(Math.random() * object.length);
+  let locindx = Math.floor(Math.random() * location.length);
+  let excuse =
+    pronoun[proindx] +
+    " " +
+    subject[subindx] +
+    " " +
+    action[actindx] +
+    " " +
+    object[objindx] +
+    " " +
+    location[locindx];
+  paragraph.textContent = excuse;
+};
